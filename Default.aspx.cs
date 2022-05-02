f856eaf63794ef98cb89ba42e2c81256cb6d4cc1
dariusdon <dariusdon18@gmail.com>
2022-05-02 16:00:13 +0300

    Functionalitate Pagina Login

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -11,8 +11,141 @@ public partial class _Default : System.Web.UI.Page
     {
 
     }
-    protected void OnClick(object sender,EventArgs e)
+    protected void OnClick(object sender, EventArgs e)
     {
-        Response.Redirect("Select.aspx");
+        string name = TextBox1.Text;
+        string password = TextBox2.Text;
+
+        if (name == "davidhadzi" && password == "61039")
+        {
+            Session["name"] = "Davidhadzi";
+            Response.Redirect("Select.aspx");
+        }
+        if (name == "preduta" && password == "61072")
+        {
+            Session["name"] = "Preduta";
+            Response.Redirect("Select.aspx");
+        }
+        if (name == "savin" && password == "61071")
+        {
+            Session["name"] = "Savin";
+            Response.Redirect("Select.aspx");
+        }
+        if (name == "anghel" && password == "60917")
+        {
+            Session["name"] = "Anghel";
+            Response.Redirect("Select.aspx");
+        }
+        if (name == "diaconu" && password == "5951")
+        {
+            Session["name"] = "Diaconu";
+            Response.Redirect("Select.aspx");
+        }
+        if (name == "jianu" && password == "6284")
+        {
+            Session["name"] = "Jianu";
+            Response.Redirect("Select.aspx");
+        }
+        if (name == "murdeala" && password == "60825")
+        {
+            Session["name"] = "Murdeala";
+            Response.Redirect("Select.aspx");
+        }
+        if (name == "brie" && password == "8189")
+        {
+            Session["name"] = "Brie";
+            Response.Redirect("Select.aspx");
+        }
+        if (name == "leu" && password == "8080")
+        {
+            Session["name"] = "Leu";
+            Response.Redirect("Select.aspx");
+        }
+        if (name == "boicu" && password == "8816")
+        {
+            Session["name"] = "Boicu";
+            Response.Redirect("Select.aspx");
+        }
+        if (name == "chitoran" && password == "4134")
+        {
+            Session["name"] = "Chitoran";
+            Response.Redirect("Select.aspx");
+        }
+        if (name == "stancu" && password == "8385")
+        {
+            Session["name"] = "Stancu";
+            Response.Redirect("Select.aspx");
+        }
+        if (name == "telegan" && password == "8161")
+        {
+            Session["name"] = "Telegan";
+            Response.Redirect("Select.aspx");
+        }
+        if (name == "beldean" && password == "4092")
+        {
+            Session["name"] = "Beldean";
+            Response.Redirect("Select.aspx");
+        }
+        if (name == "ilis" && password == "3882")
+        {
+            Session["name"] = "Ilis";
+            Response.Redirect("Select.aspx");
+        }
+
+        if (name == "lupia" && password == "61069")
+        {
+            Session["name"] = "Lupia";
+            Response.Redirect("Select.aspx");
+        }
+        if (name == "rosculet" && password == "8222")
+        {
+            Session["name"] = "Rosculet";
+            Response.Redirect("Select.aspx");
+        }
+        if (name == "suciu" && password == "8731")
+        {
+            Session["name"] = "Suciu";
+            Response.Redirect("Select.aspx");
+        }
+        if (name == "tuiu" && password == "6525")
+        {
+            Session["name"] = "Tuiu";
+            Response.Redirect("Select.aspx");
+        }
+        if (name == "csorto" && password == "8938")
+        {
+            Session["name"] = "Csorto";
+            Response.Redirect("Select.aspx");
+        }
+        if (name == "beuca" && password == "8492")
+        {
+            Session["name"] = "Beuca";
+            Response.Redirect("Select.aspx");
+        }
+        if (name == "mitiuca" && password == "8670")
+        {
+            Session["name"] = "Mitiuca";
+            Response.Redirect("Select.aspx");
+        }
+        if (name == "ivan" && password == "8585")
+        {
+            Session["name"] = "Ivan";
+            Response.Redirect("Select.aspx");
+        }
+        if (name == "golea" && password == "8877")
+        {
+            Session["name"] = "Golea";
+            Response.Redirect("Select.aspx");
+        }
+        if(name == "user" && password == "user")
+        {
+            Session["name"] = "Darius";
+            Response.Redirect("Select.aspx");
+        }
+        if(name == "admin" && password == "admin")
+        {
+            Session["name"] = "admin";
+            Response.Redirect("RaportAdmin.aspx");
+        }
     }
 }
---
+++ RaportAdmin.aspx
@@ -0,0 +1,81 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="RaportAdmin.aspx.cs" Inherits="RaportAdmin" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="margin-left: 850px; font-size: 30px; color: #ffa500;">
+            <br/>
+            <asp:Label ID="Label1" runat="server" Text="Raport Scrap:" Font-Bold="true" ></asp:Label>
+           
+            <br/>
+            <br/>
+           
+        </div>
+        
+              <asp:GridView ID="GridView1" Width="1600px" AllowPaging="true" Font-size="20px"  runat="server" HeaderStyle-BackColor="#ffa500" HeaderStyle-ForeColor="Black" PageSize="18"   >
+         
+         <PagerSettings mode="Numeric"
+          position="Bottom"           
+          pagebuttoncount="10" />
+         <PagerStyle backcolor="#ffa500"
+          height="30px"
+          verticalalign="Bottom"
+          horizontalalign="Right"/>
+     </asp:GridView>
+        
+
+            
+        
+         <div>
+             <p style="margin-top:20px;margin-left:100px">
+                <asp:Label ID="Label6" ForeColor="Orange" Font-Size="20px" runat="server" Text="Raport Per Schimb"></asp:Label>
+                <br/>
+                <asp:Label ID="Label7" ForeColor="Orange" Font-Size="20px" runat="server" Text="De la Data Si Ora:"></asp:Label>
+               &nbsp;&nbsp;
+               <asp:TextBox ID="TextBox5" TextMode="Date"  Width="150px"  runat="server"></asp:TextBox>
+                &nbsp;&nbsp;
+                 <asp:TextBox ID="TextBox6" TextMode="Time"  Width="150px"  runat="server"></asp:TextBox>
+                &nbsp;&nbsp;
+                <asp:Label ID="Label8" ForeColor="Orange" Font-Size="20px" runat="server" Text="Pana la Data Si Ora:"></asp:Label>
+                 &nbsp;&nbsp;
+               <asp:TextBox ID="TextBox7" TextMode="Date"  Width="150px"  runat="server"></asp:TextBox>
+                  &nbsp;&nbsp;
+                 <asp:TextBox ID="TextBox8" TextMode="Time" Width="150px"  runat="server"></asp:TextBox>
+                  &nbsp;&nbsp;&nbsp;
+                 <asp:DropDownList ID="DropDownList1" Font-Size="20px" runat="server" Height="30px" Width="146px">
+                      <asp:ListItem Text= "Schimb1" Value="Schib1"></asp:ListItem>
+                     <asp:ListItem Text= "Schimb2" Value="Schimb2"></asp:ListItem>
+                    <asp:ListItem Text= "Schimb3" Value="Schimb3"></asp:ListItem>
+                    
+                 </asp:DropDownList>
+                &nbsp;&nbsp;
+                <asp:Button ID="Button2" Font-Bold="true" Font-Size="20px" runat="server" Text="Genereaza Schimb" />
+             </p>
+             <p style="margin-top:20px;margin-left:100px">
+                <asp:Label ID="Label2" ForeColor="Orange" Font-Size="20px" runat="server" Text="Raport General"></asp:Label>
+                <br/>
+                <asp:Label ID="Label3" ForeColor="Orange" Font-Size="20px" runat="server" Text="De la Data Si Ora:"></asp:Label>
+               &nbsp;&nbsp;
+               <asp:TextBox ID="TextBox2" TextMode="Date"  Width="150px"  runat="server"></asp:TextBox>
+                &nbsp;&nbsp;
+                 <asp:TextBox ID="TextBox3" TextMode="Time"  Width="150px"  runat="server"></asp:TextBox>
+                &nbsp;&nbsp;
+                <asp:Label ID="Label4" ForeColor="Orange" Font-Size="20px" runat="server" Text="Pana la Data Si Ora:"></asp:Label>
+                 &nbsp;&nbsp;
+               <asp:TextBox ID="TextBox4" TextMode="Date"  Width="150px"  runat="server"></asp:TextBox>
+                  &nbsp;&nbsp;
+                 <asp:TextBox ID="TextBox9" TextMode="Time" Width="150px"  runat="server"></asp:TextBox>
+                  &nbsp;&nbsp;
+                <asp:Button ID="Button1" Font-Bold="true" Font-Size="20px" runat="server" Text="Genereaza Raport"/>
+                 <br/>
+                 <asp:Label ID="Label5" ForeColor="Orange" Font-Size="20px" runat="server" Text=""></asp:Label>
+             </p>
+        </div>
+    </form>
+</body>
+</html>
